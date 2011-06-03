4f4998d5c3c26658559cf6a49abd899b712d2fe7
Oscar Renalias <oscar@renalias.net>
2011-06-04 00:45:08 +0300

    Exercises for Clojure's day 1 in a leiningen project. Use "lein run" to run the exercises.

---
+++ clojure/day1/project.clj
@@ -0,0 +1,5 @@
+(defproject day1 "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :main "day1.code"
+)
---
+++ clojure/day1/src/day1/code.clj
@@ -0,0 +1,20 @@
+(ns day1.code)
+
+(defn big-string
+	"Returns true if the given string is longer than the given number"
+	[st n]
+	(if (> (count st) n) true false))
+
+(defn collection-type 
+	"Returns a symbol with the type of the given collection"
+	[col] 
+	(if (= (type col) clojure.lang.PersistentVector) 
+		:vector (if (= (type col) clojure.lang.PersistentArrayMap) 
+			:map (if (= (type col) clojure.lang.PersistentList) :list :dontknow))))
+
+(defn -main [& args]
+	(list 
+		(if (= (big-string "1234" 5) true) "String is longer!" "String is shorter")
+		(if (= (big-string "1234" 3) true) "String is longer!" "String is shorter")
+		(collection-type [ 1 2 3 4])
+		(collection-type (list 1 2 3 4))))
