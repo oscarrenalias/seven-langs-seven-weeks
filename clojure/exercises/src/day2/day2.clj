0c4d928e2648cf956f88df0017757ff2f3466278
Oscar Renalias <oscar@renalias.net>
2011-06-06 14:39:54 +0300

    Added exercise 1 for day 2

--- clojure/exercises/src/day2/day2.clj
+++ clojure/exercises/src/day2/day2.clj
@@ -10,10 +10,17 @@
 	Object
 	(toString [this] (str "toString")))
 	
+(defmacro unless-else 
+	"Unless macro with an else clause"
+	[expr form else-form]
+	(list 'if expr else-form form))
+	
 (defn day2
 	[]
 	; test the protocol implementation
 	(do
 		(def acc (SimpleAccount. 1000))
 		(deposit acc 50)
-		(withdraw acc 500)))
+		(withdraw acc 500)
+		(unless-else true (println "This should not print") (println "This should print"))
+		(unless-else false (println "This should print") (println "This should not print"))))
