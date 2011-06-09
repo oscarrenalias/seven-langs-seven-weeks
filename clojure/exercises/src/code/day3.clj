980ee6f823edcd02b064e45dbdca821296f39704
Oscar Renalias <oscar@renalias.net>
2011-06-10 00:58:03 +0300

    Added my solution to the barber's shop problem

--- clojure/exercises/src/code/day3.clj
+++ clojure/exercises/src/code/day3.clj
@@ -25,33 +25,36 @@
 
 ; this defines the number of seats available at the barber's shop
 (def seats 3)
+(def barber (agent 0))
 
-(defn init-shop [] (ref []))
-
+; Adds a customer to the queue if there's an emtpy seat, or else send the customer away
 (defn receive-customer
 	[q cid]
 	(msg "Receiving customer: " cid)
 	(dosync 
 		(if (< (count @q) seats)
-			(alter q conj cid)
+			(do (alter q conj cid) (send-off barber cut-hair q))
 			(msg "Sending customer away: " cid))))
-			
+	
+; does a haircut		
 (defn cut-hair
-	[q]
+	[total q]
 	(dosync 
 		(if (> (count @q) 0)
-			(
-				(msg "Cutting hair of customer: " )
-				(Thread/sleep (rand-int 100))
-				(alter q rest))
+			(do 
+				(msg "Cutting hair of customer: " (first @q)) 
+				(Thread/sleep (rand-int 100)) 
+				(alter q next) (inc total))
 			(msg "No customers left!"))))
 			
+; runs the shop for some time
 (defn run-shop 
 	[q]
 	(doseq [c (range 1 200)]
 		(Thread/sleep (rand-int 100))
 		(future (receive-customer q c))))
 			
+; can be used to run the demo
 (defn day3 [] 
-	(def shop (init-shop))
-	(run-shop shop))
+	(let [shop (ref [])]
+		(do (run-shop shop) (msg "Total number of haircuts today " (str @barber)))))
